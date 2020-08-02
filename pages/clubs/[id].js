e440838e9de78bc2d65e2a9b1fb6ece191d46183
achour <achourmedhedi@gmail.com>
2020-08-02 12:28:34 +0100

    test

--- pages/clubs/[id].js
+++ pages/clubs/[id].js
@@ -1,20 +1,20 @@
-// import Head from "next/head";
+import Head from "next/head";
 import fetch from 'isomorphic-unfetch'
-// import css from '../../shared/css/clubWebsite.scss'
-// import Navbar from '../../shared/components/Website/Navbar/index'
-// import Introduction from '../../shared/components/Website/Introduction/index'
-// import Details from '../../shared/components/Website/Details/index'
-// import Staff from '../../shared/components/Website/Staff/index'
-// import Price from '../../shared/components/Website/Price/index'
-// import Partners from '../../shared/components/Website/Partners/index'
-// import Contact from '../../shared/components/Website/Contact/index'
-// import globalCss from '../../shared/global-style.scss'
-// import logoblanc from '../../public/icon/logobanc.svg'
-// import Link from "next/link";
-// import GlobalNavbar from '../../shared/components/navbar/Navbar';
+import css from '../../shared/css/clubWebsite.scss'
+import Navbar from '../../shared/components/Website/Navbar/index'
+import Introduction from '../../shared/components/Website/Introduction/index'
+import Details from '../../shared/components/Website/Details/index'
+import Staff from '../../shared/components/Website/Staff/index'
+import Price from '../../shared/components/Website/Price/index'
+import Partners from '../../shared/components/Website/Partners/index'
+import Contact from '../../shared/components/Website/Contact/index'
+import globalCss from '../../shared/global-style.scss'
+import logoblanc from '../../public/icon/logobanc.svg'
+import Link from "next/link";
+import GlobalNavbar from '../../shared/components/navbar/Navbar';
 
 
-const ClubWebsite = ({ status = 401 , data, clubName, logo }) => {
+const ClubWebsite = ({ status , data, clubName, logo }) => {
 
 
 
@@ -34,41 +34,40 @@ const ClubWebsite = ({ status = 401 , data, clubName, logo }) => {
         )
     }
 
-    // if (status === 200) {
-    //     return (
-    //         <div className={css.clubWebsite}>
-    //             <Head>
-    //                 <title>{clubName}</title>
+    if (status === 200) {
+        return (
+            <div className={css.clubWebsite}>
+                <Head>
+                    <title>{clubName}</title>
 
-    //                 <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-    //                 <meta name="description" content={data.introduction.body.title} />
-    //                 <meta name="keywords" content={`${clubName} , sport, clubs, ${data.introduction.body.sport}, ${data.partners.body.map(el => el.name + ", ")}`} />
-    //                 <meta name="author" content="iSporit" />
+                    <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+                    <meta name="description" content={data.introduction.body.title} />
+                    <meta name="keywords" content={`${clubName} , sport, clubs, ${data.introduction.body.sport}, ${data.partners.body.map(el => el.name + ", ")}`} />
+                    <meta name="author" content="iSporit" />
 
-    //             </Head>
-    //             <div className={css.clubWebsite__body}>
-    //                 <div className={css.headerblock}>
-    //                     <Link href="/" >
-    //                         <a>
-    //                             <img className={css.headerblock__img} src={logoblanc} alt="logo" />
-    //                         </a>
-    //                         {/* <span className={css.headerblock__connexion}>Connexion</span> */}
-    //                     </Link>
-    //                 </div>
-    //                 {/* <Navbar data={data} logo={logo} />
-    //                 <Introduction data={data.introduction} />
-    //                 <Details data={data.details} />
-    //                 <Staff data={data.staff} />
-    //                 <Price data={data.prices} />
-    //                 <Partners data={data.partners} />
-    //                 <Contact data={data.contact} />
-    //                 <GlobalNavbar isFooter /> */}
-    //             </div>
-    //         </div>
-    //     )
-    // }
+                </Head>
+                <div className={css.clubWebsite__body}>
+                    <div className={css.headerblock}>
+                        <Link href="/" >
+                            <a>
+                                <img className={css.headerblock__img} src={logoblanc} alt="logo" />
+                            </a>
+                            {/* <span className={css.headerblock__connexion}>Connexion</span> */}
+                        </Link>
+                    </div>
+                    <Navbar data={data} logo={logo} />
+                    <Introduction data={data.introduction} />
+                    <Details data={data.details} />
+                    <Staff data={data.staff} />
+                    <Price data={data.prices} />
+                    <Partners data={data.partners} />
+                    <Contact data={data.contact} />
+                    <GlobalNavbar isFooter />
+                </div>
+            </div>
+        )
+    }
 
-    return 'it works'
 }
 
 ClubWebsite.getInitialProps = async (ctx) => {
