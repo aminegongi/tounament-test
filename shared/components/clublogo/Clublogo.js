e086497440c44e84dcc56e51c4144fbfbe1baf4b
achour <achourmedhedi@gmail.com>
2021-02-16 15:49:40 +0100

    delete page

--- shared/components/clublogo/Clublogo.js
+++ shared/components/clublogo/Clublogo.js
@@ -8,16 +8,16 @@ export default function Clublogo({ images }) {
     return (
         <div className={`${globalCss.isporit_max_width}`}>
             <div className={css.clubs}>
-                Clubs partenaires nous font confiance
+                Les clubs nous font confiance
             </div>
-            <Link href={routes.SIGN_UP.path} >
-                <div className={css.img_club}>
-                    {
-                        images.map(el => <img key={Math.round(Math.random() * Math.random() * 100)}
-                            alt={el.alt} className={css.image} src={el.src} />)
-                    }
-                </div>
-            </Link>
+            {/* <Link href={routes.SIGN_UP.path} > */}
+            <div className={css.img_club}>
+                {
+                    images.map(el => <img key={Math.round(Math.random() * Math.random() * 100)}
+                        alt={el.alt} className={css.image} src={el.src} />)
+                }
+            </div>
+            {/* </Link> */}
         </div>
     );
 }
--- shared/components/navbar/Navbar.js
+++ shared/components/navbar/Navbar.js
@@ -187,13 +187,13 @@ function NavbarIndex({ buttontwo, isFooter }) {
               </div>
             </div>
 
-            <div className={css.item}>
+            {/* <div className={css.item}>
               <Link href={routes.SEARCH_CLUB.path}>
                 <a>
                   Clubs partenaires
                 </a>
               </Link>
-            </div>
+            </div> */}
             {/* <div className={css.item}>
             Chercher un joueur
           </div> */}
