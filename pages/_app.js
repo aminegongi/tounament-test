c848dfec8136b262bb6eaf8a2f465109944e1282
achour <achourmedhedi@gmail.com>
2021-04-15 18:07:30 +0100

    pwa

--- pages/_app.js
+++ pages/_app.js
@@ -55,18 +55,16 @@ function MyApp({ Component, pageProps }) {
     setIsLoginModalOpen(false)
   }, [pathname])
   useEffect(() => {
-    // if ('serviceWorker' in navigator) {
-
-    //   navigator.serviceWorker
-    //   .register('/service-worker.js')
-    //   .then(registration => {
-    //     console.log('service worker registration successful')
-    //   })
-    //   .catch(err => {
-    //     console.warn('service worker registration failed', err.message)
-    //   })
-
-    // }
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker
+        .register('/service-worker.js')
+        .then((registration) => {
+          console.log('service worker registration successful')
+        })
+        .catch((err) => {
+          console.warn('service worker registration failed', err.message)
+        })
+    }
     fetchProfile()
     hotjar.initialize(1813801, 6)
 
