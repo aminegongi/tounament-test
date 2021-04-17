15971bd187d87666ca4b84b14cd42de1f13109a8
achour <achourmedhedi@gmail.com>
2021-04-17 17:29:42 +0100

    messages

--- pages/sign-up.js
+++ pages/sign-up.js
@@ -247,7 +247,7 @@ const SignUp = () => {
                   className={data.userType === 'player' && 'radio_group_button'}
                   value="player"
                 >
-                  Joueur/Autre
+                  Adherent/Joueur
                 </Radio.Button>
                 <Radio.Button
                   className={data.userType === 'coach' && 'radio_group_button'}
