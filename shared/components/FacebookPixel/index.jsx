e134cbf5a9874a0d36452389bb932a1191d37673
achour <achourmedhedi@gmail.com>
2021-04-23 17:45:04 +0100

    pixel

--- shared/components/FacebookPixel/index.jsx
+++ shared/components/FacebookPixel/index.jsx
@@ -34,7 +34,7 @@ const FacebookPixel = ({ children }) => {
                 t.src=v;s=b.getElementsByTagName(e)[0];
                 s.parentNode.insertBefore(t,s)}(window, document,'script',
                 'https://connect.facebook.net/en_US/fbevents.js');
-                fbq('init', '${FB_PIXEL_ID}');
+                fbq('init', ${FB_PIXEL_ID});
                 fbq('track', 'PageView');
                 
               `,
