3e316d2f5cb6b8fcf7a3c2ba58de1a5cfe817150
achour <achourmedhedi@gmail.com>
2022-03-12 08:35:58 +0100

    salem

--- pages/404.js
+++ pages/404.js
@@ -1,14 +1,10 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react'
-import Layout from '../shared/components/layout/Layout'
-import Error from '../shared/components/PageError'
+// import Layout from '../shared/components/layout/Layout'
+// import Error from '../shared/components/PageError'
 
 function NotFound() {
-  return (
-    <Layout>
-      <Error statusCode={404} description="Oops!!!" />
-    </Layout>
-  )
+  return 'error'
 }
 
 export default NotFound
